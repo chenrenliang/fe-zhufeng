43e99c8f0968f3a1b8bd1b22834259a538d1bfea
solaris chen <474571921@qq.com>
2024-03-22 00:19:42 +0800

    146-browser

---
+++ 146-browser/client/main.js
@@ -0,0 +1,11 @@
+const EventEmitter = require('events');
+
+class GPU extends EventEmitter {
+    constructor() {
+        super();
+        this.bitMaps = [];
+    }
+}
+
+const gpu = new GPU();
+module.exports = gpu;
